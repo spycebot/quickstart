f917f63d89833153ee3c426e62acf204b7d85356
spycebot <photius@shannonware.com>
2017-06-19 21:37:56 +0100

    Complete tutorial up to router outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,33 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }	from '@angular/forms' //NgModle lives here
+import { NgModule }      		from '@angular/core';
+import { BrowserModule } 		from '@angular/platform-browser';
+import { FormsModule }			from '@angular/forms' //NgModule lives here
+import { RouterModule } 		from '@angular/router';
 
-import { HeroDetailComponent } from './hero-detail.component';
-import { AppComponent }  from './app.component';
+import { AppComponent }			from './app.component';
+import { HeroDetailComponent }	from './hero-detail.component';
+import { HeroesComponent } 		from './heroes.component';
+import { HeroService } 			from './hero.service';
+
+
+
 
 @NgModule({
   imports:      [ 
   	BrowserModule,
-  	FormsModule // import the FormsModule before binding with [(ngModel)]
+  	FormsModule, // import the FormsModule before binding with [(ngModel)]
+	RouterModule.forRoot([
+		{
+			path: 'heroes',
+			component: HeroesComponent
+		}
+	])
   ],
   declarations: [ 
   	AppComponent,
-  	HeroDetailComponent
+  	HeroDetailComponent,
+  	HeroesComponent
   ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [ HeroService ]
 })
 export class AppModule { }
